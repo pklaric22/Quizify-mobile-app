eb56c9f3c7e8a6d559a41c9bae6d60032167e307
pklaric22 <pklaric22@student.foi.hr>
2024-11-14 11:15:35 +0100

    Testni comit 3

--- Software/app/src/main/java/com/example/quizifyrampu/MainActivity.kt
+++ Software/app/src/main/java/com/example/quizifyrampu/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hellooo! $name!",
+        text = "Helloooooooooo123! $name!",
         modifier = modifier
     )
 }
